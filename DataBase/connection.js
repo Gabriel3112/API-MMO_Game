fb0e43675d66488f3cddcfe30a537d1f07eee366
Gabriel3112 <gabriellima678.gl@gmail.com>
2020-10-31 02:08:10 -0300

    new database

--- DataBase/connection.js
+++ DataBase/connection.js
@@ -1,10 +1,10 @@
 var MongoClient = require('mongodb').MongoClient;
-var url = process.env.DATABASE_CONNECTION;
+var url = "mongodb+srv://admin:88170591Ga@clusterunity.5ofjs.gcp.mongodb.net/database_players?retryWrites=true&w=majority";
 
   MongoClient.connect(url, { useUnifiedTopology: true },(err, db)=>{
     if(err) throw err;
     console.log('Database Connected!')
-    var dbo = db.db(process.env.DATABASE_USER);
-    var collectionUsers = dbo.collection(process.env.DATABASE_COLLECTION_ONE);
+    var dbo = db.db('database_players');
+    var collectionUsers = dbo.collection('players');
     exports.coll={collectionUsers:collectionUsers, db:db};
   });
